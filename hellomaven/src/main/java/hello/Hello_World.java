84b64e4974c901273c3a29e4ddaa55e020f7cb6f
Anju-Kurian-08 <150518861+Anju-Kurian-08@users.noreply.github.com>
2023-11-15 08:17:28 +0530

    Mymaven

---
+++ hellomaven/src/main/java/hello/Hello_World.java
@@ -0,0 +1,13 @@
+package hello;
+
+public class Hello_World {
+	
+
+
+	public static void main(String[] args) {
+		
+		System.out.println("this is my maven project");		// TODO Auto-generated method stub
+
+	}
+
+}
